749adf7fe34f2e15170233434ebe018c611da960
farshad-1988 <96774694+farshad-1988@users.noreply.github.com>
2025-10-27 02:18:54 +0330

    fix:add some code to next.config.ts to accept image url from other sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // allow all hosts
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       destination: "http://localhost:5000/api/:path*",
+  //     },
+  //   ];
+  // },
 };
-
 export default nextConfig;
