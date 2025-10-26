731f629ad2e2b212904ea9a8405346a358df8c62
farshad-1988 <96774694+farshad-1988@users.noreply.github.com>
2025-10-26 23:05:49 +0330

    fix:add next-auth.d.ts file to add type to next-auth in order to add some property to session

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -10,13 +10,38 @@ export type Item = {
 };
 
 export type CartItem = Item & { count: number };
+export type productItem = Item & { stock: number };
+export type CartItems = CartItem[];
+
+export type OrdersItemStore = {
+  // loading: boolean;
+  // error: string | null;
+  orders: CartItems;
+  setOrders: (orders: CartItems) => void;
+  increaseOrderedItem: (item: Item) => void;
+  decreaseOrderedItem: (item: Item) => void;
+};
+export type ProductsItemStore = {
+  // loading: boolean;
+  // error: string | null;
+  products: productItem[];
+  filteredProducts: productItem[];
+  setProducts: (products: productItem[]) => void;
+  setFilteredProducts: (products: productItem[]) => void;
+  increaseProductsItem: (item: productItem) => void;
+  decreaseProductsItem: (item: productItem) => void;
+};
+export type UserItemStore = {
+  // loading: boolean;
+  // error: string | null;
+  user: User;
+  setUser: (user: User) => void;
+};
 
 export enum UserRole {
   ADMIN = "admin",
   CUSTOMER = "customer",
 }
-
-export type CartItems = CartItem[];
 
 export interface User {
   id: string;
@@ -25,4 +50,30 @@ export interface User {
   createdAt?: string;
   orders: CartItems[];
   role: UserRole;
+}
+
+export interface AuthFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface AuthRequest {
+  action: "signin" | "signup";
+  email: string;
+  password: string;
+  name?: string;
+}
+
+export interface AuthResponse {
+  success: boolean;
+  user?: User;
+  error?: string;
+}
+
+export enum SnackbarSeverityEnum {
+  Success = "success",
+  Error = "error",
+  Info = "info",
+  Warning = "warning",
 }
