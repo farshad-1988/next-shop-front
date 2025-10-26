f1073bfebec0a8a0dc0c7f583cf72f8edbc5c807
farshad-1988 <96774694+farshad-1988@users.noreply.github.com>
2025-10-27 02:32:03 +0330

    style:modify footer

--- src/app/(layoutsComp)/Footer.tsx
+++ src/app/(layoutsComp)/Footer.tsx
@@ -12,6 +12,7 @@ const Footer = () => {
         background: "linear-gradient(45deg, #667eea 30%, #764ba2 90%)",
         boxShadow: "0 3px 15px rgba(102, 126, 234, 0.4)",
         height: "10%",
+        marginTop: "auto",
       }}
     >
       <p>&copy; {new Date().getFullYear()} SHOP APP</p>
